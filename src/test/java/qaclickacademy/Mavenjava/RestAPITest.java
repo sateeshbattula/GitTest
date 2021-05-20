6d8137fa6b55b0a23a24fa880bceb401beefa1b6
Sateesh <insbattula@ad.harman.com>
2021-05-20 19:05:12 +0530

    Us guy dine changes

--- src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -9,6 +9,12 @@ public class RestAPITest {
 		System.out.println("PostJira");
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
+		
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
+		System.out.println("PostJira7");
+		
 	}
 	@Test
 	public void deleteTwitter() {
--- src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -8,6 +8,7 @@ public class SeleniumTest {
 	public void BrowserAutomation() {
 		
 		System.out.println("BrowserAutomation");
+		System.out.println("Web Automation");
 		
 	}
 	@Test
